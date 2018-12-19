ceb16d7d35e32994ccb4d1403cabef9005ecea02
Zheyuan Xie <zyxie.sjtu@gmail.com>
2018-12-19 02:42:26 -0500

    Use faceplusplus for landmark detection

--- getLandmarks.py
+++ getLandmarks.py
@@ -6,13 +6,46 @@ Date created: 2018-12-18
 
 import dlib
 import numpy as np
+from PythonSDK.facepp import API,File
+from pprint import pformat
+import cv2
 
+# dlib detector and predictor
 PREDICTOR_PATH = "shape_predictor_68_face_landmarks.dat"
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(PREDICTOR_PATH)
+
+# faceplusplus API and landmark notation
+api = API()
+f = open('landmark83.txt')
+landmark_name = f.readline().split(',')
+f.close()
 
 def get_landmarks(im):
     rects = detector(im, 1)
     if len(rects) is not 1:     # if no face or multiple faces, return None
         return None
     return np.array([[p.x, p.y] for p in predictor(im, rects[0]).parts()])
+
+def get_landmarks_facepp(img):
+    cv2.imwrite('temp.jpg',img)
+    res = api.detect(image_file=File('./temp.jpg'), return_landmark=1)
+    lm = np.zeros((83,2))
+    for i in range(83):
+        point = res['faces'][0]['landmark'][landmark_name[i]]
+        lm[i,0] = point['x']
+        lm[i,1] = point['y']
+    return lm
+
+if __name__ == "__main__":
+    from loader import loadvideo
+    filename1 = 'CIS581Project4PartCDatasets/Easy/FrankUnderwood.mp4'
+    print('loading...')
+    video = loadvideo(filename1)
+    print('done')
+    img = video[0]
+    lm = get_landmarks_facepp(img)
+    for groups in lm:
+        cv2.circle(img, (int(groups[0]),int(groups[1])), 3, (255, 0, 0), 2)
+    cv2.imshow('img',img)
+    cv2.waitKey(0)
--- loader.py
+++ loader.py
@@ -6,7 +6,7 @@ Date created: 2018-12-18
 
 import pickle
 import cv2
-from getLandmarks import get_landmarks
+from getLandmarks import get_landmarks_facepp, get_landmarks
 
 def loadvideo(filename):
     cap = cv2.VideoCapture(filename)
@@ -32,20 +32,44 @@ def savelandmarks(filename):
     with open(filename.split('.')[0]+'.landmarks', 'wb') as f:
         pickle.dump(landmarks, f)
 
+def savelandmarks_facepp(filename):
+    video = loadvideo(filename)
+    landmarks = []
+    print('Number of Frames: %d'%(len(video)))
+    cnt = 0
+    for image in video:
+        landmarks.append(get_landmarks_facepp(image))
+        cnt  = cnt + 1
+        print('%f%%'%(cnt/len(video)*100))
+    # Dump landmarks to file
+    with open(filename.split('.')[0]+'.facepp', 'wb') as f:
+        pickle.dump(landmarks, f)
+
 def loadlandmarks(filename):
     # Load landmarks from file
     with open(filename.split('.')[0]+'.landmarks', 'rb') as f:
         landmarks = pickle.load(f)
     return landmarks
 
-def vislandmarks(filename, play = False):
+def loadlandmarks_facepp(filename):
+    # Load landmarks from file
+    with open(filename.split('.')[0]+'.facepp', 'rb') as f:
+        landmarks = pickle.load(f)
+    return landmarks
+
+def vislandmarks(filename, play = False, use_facepp = False):
     video = loadvideo(filename)
+    if use_facepp:
+        landmarks_facepp = loadlandmarks_facepp(filename)
     landmarks = loadlandmarks(filename)
     n_frames = len(video)
     for i in range(n_frames):
         if landmarks[i] is not None:
-            for groups in landmarks[i]:
+            for groups in landmarks[i].astype(int):
                 cv2.circle(video[i], (groups[0],groups[1]), 3, (255, 0, 0), 2)
+        if use_facepp:
+            for groups in landmarks_facepp[i].astype(int):
+                cv2.circle(video[i], (groups[0],groups[1]), 3, (0, 255, 0), 2)
         if play:
             cv2.imshow('Landmarks',video[i])
             cv2.waitKey(50)
@@ -58,6 +82,6 @@ if __name__ == "__main__":
     # savelandmarks(filename1)
     # savelandmarks(filename2)
     # savelandmarks(filename3)
-    vislandmarks(filename1)
-    vislandmarks(filename2)
-    vislandmarks(filename3)
+    # vislandmarks(filename1, play=True)
+    # vislandmarks(filename2, play=True, use_facepp=True)
+    vislandmarks(filename3, play=True, use_facepp=True)
